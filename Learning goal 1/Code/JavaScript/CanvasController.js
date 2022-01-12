cebb6bb780c5dcc2f63b9645fa42ef70174bbada
Nils Meijer <meijernils9@gmail.com>
2022-01-12 15:27:46 +0100

    Implemented moving UI

---
+++ Learning goal 1/Code/JavaScript/CanvasController.js
@@ -0,0 +1,52 @@
+export default class CanvasController {
+    window;
+    canvas;
+    canvasWidth;
+    canvasHeight;
+  
+    home_element;
+    home_style;
+  
+    portfolio_element;
+    portfolio_style;
+    
+    aboutme_element;
+    aboutme_style;
+    
+    contact_me_element;
+    contact_me_style;
+  
+    delta = 0;
+  
+    foregroundSpeed;
+  
+    constructor(document, window) {
+      this.document = document;
+      this.window = window;
+      this.onInit();
+    }
+  
+    onInit() {
+      this.document.onload = this.getHTML();
+    }
+  
+    getHTML() {
+      this.home_element = document.getElementById('home-intro');
+      this.home_style = window.getComputedStyle(this.home_element);
+      this.setPositionReferenceValues();
+    }
+  
+    setPositionReferenceValues(){
+      this.xPosHome = this.home_style.left;
+    }
+    
+    moveUI(progress, direction) {
+      if(direction == 0) return;
+      if (direction == -1) {
+          this.delta += this.foregroundSpeed * progress;
+      } else this.delta -= this.foregroundSpeed * progress;
+
+      this.home_element.style.left = `${parseInt(this.xPosHome, 10) + this.delta}px`;
+    }
+  }
+  
--- Learning goal 1/Code/JavaScript/main.js
+++ Learning goal 1/Code/JavaScript/main.js
@@ -1,198 +1,5 @@
-// import Player from "./Player.js";
-
+import CanvasController from "./CanvasController.js";
 import Player from "./Player.js";
-
-// let physicsScene, mainScene, camera, renderer, light;
-// let player;
-// let clock = new THREE.Clock();
-// let delta = 0;
-// let timeStep;
-
-// let playerBody;
-// let obstacleMaterial;
-
-// function prepareAll() {
-//   prepareCannon();
-//   prepareThree();
-//   prepareGFX();
-//   document.body.appendChild(renderer.domElement);
-//   window.addEventListener("resize", onWindowResize, false);
-
-//   initializeTestShape();
-// }
-
-// function prepareGFX() {
-//   prepareLevel();
-//   preparePlayer();
-// }
-
-// function prepareThree() {
-//   mainScene = new THREE.Scene();
-
-//   camera = new THREE.PerspectiveCamera(
-//     30,
-//     window.innerWidth / window.innerHeight,
-//     0.1,
-//     1000
-//   );
-
-//   renderer = new THREE.WebGLRenderer({ antialias: true });
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-//   renderer.shadowMap.enabled = true;
-//   renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-
-//   light = new THREE.DirectionalLight(0xffffff, 3.5);
-//   light.position.set(-6, 1, 0);
-//   light.castShadow = true;
-//   light.shadowCameraVisible = true;
-
-//   mainScene.add(light);
-
-//   camera.position.z = 15;
-//   camera.position.y = 5;
-// }
-
-// function prepareCannon() {
-//   physicsScene = new CANNON.World();
-//   physicsScene.gravity.set(0, -9.82, 0);
-//   physicsScene.broadphase = new CANNON.NaiveBroadphase();
-
-//   var mass = 5,
-//     radius = 1;
-//   var sphereShape = new CANNON.Sphere(radius);
-//   var sphereRB = new CANNON.Body({ mass: mass, shape: sphereShape });
-//   sphereRB.position.set(0, 0, 0);
-//   physicsScene.addBody(sphereRB);
-
-//   var groundShape = new CANNON.Plane();
-//   var groundBody = new CANNON.Body({ mass: 0, shape: groundShape });
-//   physicsScene.addBody(groundBody);
-
-//   timeStep = 1.0 / 60.0;
-//   physicsScene.solver.iterations = 10;
-
-//   var physicsMaterial = new CANNON.Material("groundMaterial");
-//   var physicsContactMaterial = new CANNON.ContactMaterial(
-//     physicsMaterial,
-//     physicsMaterial
-//   );
-
-//   physicsScene.addContactMaterial(physicsContactMaterial);
-
-//   var otherMaterial = new CANNON.Material("slipperyMaterial");
-
-//   var anotherContactMaterial = new CANNON.ContactMaterial(
-//     physicsMaterial,
-//     otherMaterial,
-//     0.0,
-//     0.9
-//   );
-
-//   physicsScene.addContactMaterial(anotherContactMaterial);
-
-//   var halfExtends = new CANNON.Vec3(0.25, 0.5, 0.25);
-//   var boxShape = new CANNON.Box(halfExtends);
-
-//   var boxGeometry = new THREE.BoxGeometry(
-//     halfExtends.x * 2,
-//     halfExtends.y * 2,
-//     halfExtends.z * 2
-//   );
-
-//   playerBody = new CANNON.Body(5, boxShape);
-//   playerBody.addShape(boxShape);
-//   playerBody.material = anotherContactMaterial;
-//   physicsScene.addBody(playerBody);
-// }
-
-// function initializeTestShape() {
-//   const normalMaterial = new THREE.MeshNormalMaterial();
-//   const phongMaterial = new THREE.MeshPhongMaterial();
-//   obstacleMaterial = new THREE.MeshStandardMaterial();
-//   obstacleMaterial.color.setHex(0x2305fd);
-
-//   const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-//   const cubeMesh = new THREE.Mesh(cubeGeometry, obstacleMaterial);
-//   cubeMesh.position.x = -3;
-//   cubeMesh.position.y = 0;
-//   cubeMesh.castShadow = true;
-
-//   mainScene.add(cubeMesh);
-
-//   const cubeShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5));
-//   const cubeBody = new CANNON.Body({mass: 1});
-//   cubeBody.addShape(cubeShape);
-//   cubeBody.position.x = cubeMesh.position.x;
-//   cubeBody.position.y = cubeMesh.position.y;
-//   cubeBody.position.z = cubeMesh.position.z;
-//   physicsScene.addBody(cubeBody);
-
-//   console.log(cubeBody);
-// }
-
-// function animate() {
-//   delta = Math.min(clock.getDelta(), 0.1)
-//   physicsScene.step(delta);
-//   requestAnimationFrame(animate);
-
-//   update(delta);
-
-//   renderer.render(mainScene, camera);
-// }
-
-// function onWindowResize() {
-//   camera.aspect = window.innerWidth / window.innerHeight;
-//   camera.updateProjectionMatrix();
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-// }
-
-// function prepareLevel() {
-//   const groundGeo = new THREE.BoxGeometry(100, 0.5, 4);
-//   const groundMat = new THREE.MeshStandardMaterial();
-//   const ground = new THREE.Mesh(groundGeo, groundMat);
-//   ground.receiveShadow = true;
-//   mainScene.add(ground);
-//   ground.position.y = -3.5;
-// }
-
-// function preparePlayer() {
-//   player = new Player(4);
-//   mainScene.add(player);
-//   document.addEventListener("keydown", function (event) {
-//     if (event.key == "a" || event.key == "A") player.movingLeft = true;
-//     if (event.key == "d" || event.key == "D") player.movingRight = true;
-//   });
-//   document.addEventListener("keyup", function (event) {
-//     if (event.key == "a" || event.key == "A") player.movingLeft = false;
-//     if (event.key == "d" || event.key == "D") player.movingRight = false;
-//   });
-
-//   document.addEventListener("keydown", function (event) {
-//     if (event.key == "space") player.movingLeft = true;
-//   });
-// }
-
-// function update(delta) {
-//   //followPlayer();
-//   playerBody.position = player.position;
-//   player.update(delta);
-//   camera.lookAt(player.position);
-// }
-
-// function followPlayer() {
-//   camera.position.x = player.position.x;
-// }
-
-// window.onload = function () {
-//   prepareAll();
-//   animate();
-// };
-
-// import * as THREE from 'three'
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-// import Stats from 'three/examples/jsm/libs/stats.module'
-// import { GUI } from 'dat.gui'
-// import * as CANNON from 'cannon-es'
 
 const scene = new THREE.Scene();
 scene.add(new THREE.AxesHelper(5));
@@ -200,6 +7,8 @@ scene.add(new THREE.AxesHelper(5));
 let obstacleMaterial, playerMaterial, groundMaterial;
 let playerInstance;
 let cubeMesh, cubeBody;
+const canvasController = new CanvasController(document, window);
+let moveDirection = 0;
 
 const light1 = new THREE.PointLight();
 light1.position.set(0, 3, 0);
@@ -230,7 +39,7 @@ obstacleMaterial = new THREE.MeshPhongMaterial();
 obstacleMaterial.color.setHex(0xb905fd);
 
 groundMaterial = new THREE.MeshPhongMaterial();
-groundMaterial.color.setHex(0x003B0C);
+groundMaterial.color.setHex(0x003b0c);
 
 function createGround() {
   const planeGeometry = new THREE.PlaneGeometry(40, 10);
@@ -249,23 +58,35 @@ function createGround() {
 
 function createPlayer() {
   playerInstance = new Player(2, 3);
+  canvasController.foregroundSpeed = playerInstance.defaultMoveSpeed * 100;
   physicsWorld.addBody(playerInstance.playerBody);
   scene.add(playerInstance.playerMesh);
 
   document.addEventListener("keydown", function (event) {
-    if (event.key == "a" || event.key == "A") playerInstance.movingLeft = true;
-    if (event.key == "d" || event.key == "D") playerInstance.movingRight = true;
+    if (event.key == "a" || event.key == "A") {
+      playerInstance.movingLeft = true;
+      moveDirection = -1;
+    }
+    if (event.key == "d" || event.key == "D") {
+      playerInstance.movingRight = true;
+      moveDirection = 1;
+    }
   });
   document.addEventListener("keyup", function (event) {
-    if (event.key == "a" || event.key == "A") playerInstance.movingLeft = false;
-    if (event.key == "d" || event.key == "D")
+    if (event.key == "a" || event.key == "A") {
+      playerInstance.movingLeft = false;
+      moveDirection = 0;
+    }
+    if (event.key == "d" || event.key == "D") {
       playerInstance.movingRight = false;
+      moveDirection = 0;
+    }
   });
 
   document.addEventListener("keydown", function (event) {
     if (event.key == " ") {
       playerInstance.isJumping = true;
-      console.log("Jumping")
+      console.log("Jumping");
     }
   });
 }
@@ -307,6 +128,12 @@ function initialize() {
   createGround();
 }
 
+let cameraOffsetX = 3.5;
+function cameraFollowPlayer() {
+  //camera.lookAt(playerInstance.position);
+  camera.position.x = playerInstance.playerBody.position.x + cameraOffsetX;
+}
+
 function animate() {
   requestAnimationFrame(animate);
 
@@ -327,6 +154,8 @@ function animate() {
   );
 
   playerInstance.update(delta);
+  canvasController.moveUI(delta, moveDirection);
+  cameraFollowPlayer();
 
   render();
 }
